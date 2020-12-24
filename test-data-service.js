71045f6489e3eef27744e53a781af2978257b98c
Hackagon <phonghiavan@gmail.com>
2020-12-24 22:38:48 +0700

    handle data

---
+++ test-data-service.js
@@ -0,0 +1,21 @@
+const dataService = require("./lib/data-service")
+
+// dataService.create("test", "newFile", { foo: "bar" }, (err, message) => {
+//   if (err) return console.log(err)
+//   return console.log(message)
+// })
+
+// dataService.read("test", "newFile", (err, data) => {
+//   if (err) return console.log(err)
+//   return console.log(data)
+// })
+
+// dataService.update("test", "newFile", { test: "test" }, (err, data) => {
+//   if (err) return console.log(err)
+//   return console.log(data)
+// })
+
+// dataService.delete("test", "newFile", (err, message) => {
+//   if (err) return console.log(err)
+//   return console.log(message)
+// })
